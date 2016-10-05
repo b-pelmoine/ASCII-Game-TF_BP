cb58d3b3cddc3fad76e6a0a5d0b170cf4d2f24dc
thomasfrancisco <thofrancisco@gmail.com>
2016-10-05 13:56:49 +0200

    Quick definition of GameObject

--- Project1/GameObject.cpp
+++ Project1/GameObject.cpp
@@ -6,7 +6,19 @@ GameObject::GameObject()
 {
 }
 
+GameObject::GameObject(float posX, float posY, float sizeX, float sizeY, char * ascii) : m_posX(posX), m_posY(posY), m_sizeX(sizeX), m_sizeY(sizeY), m_ascii(ascii)
+{
+}
+
 
 GameObject::~GameObject()
+{
+}
+
+void GameObject::move(void)
+{
+}
+
+void GameObject::display(void)
 {
 }
--- Project1/GameObject.h
+++ Project1/GameObject.h
@@ -3,6 +3,18 @@ class GameObject
 {
 public:
 	GameObject();
+	GameObject(float posX, float posY, float sizeX, float sizeY, char * ascii);
 	~GameObject();
+	void move(void);
+	void display(void);
+
+
+
+private:
+	float m_posX;
+	float m_posY;
+	short m_sizeX;
+	short m_sizeY;
+	char * m_ascii;
 };
 
